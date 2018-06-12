6d61476b0668fe8fc19276da0d9b62eaac676554
EBien-Aime <ebienaime@uarts.edu>
2018-06-12 14:57:46 -0400

    user sign ups & sign ins

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  	def current_user
+  		if session[:user_id]
+  		    @current_user = User.find(session[:user_id])
+  		end
+  	end
+
+  	def logged_in?
+  		current_user != nil
+  	end
+
+  	helper_method :current_user, :logged_in?
+
 end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,65 @@
+class UsersController < ApplicationController
+
+  def index
+  	@users = User.all
+  end
+
+  def show
+  	@user = User.find(params[:id])
+  end
+
+  def new
+  	@user = User.new
+  end
+
+
+  def create
+  	user = User.new(user_params)
+  	if user.save
+  		redirect_to "/"
+  	else
+  		render "/users/new"
+  	end
+  end
+
+  def signin
+    @user = User.find_by_name(params[:name])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to "/"
+    else
+      redirect_to :controller => 'sessions', :action => 'new'
+    end
+  end
+
+  def edit
+  	@user = User.find(params[:id])
+  end
+
+  def update
+    user = User.find(params[:id])
+    if(user.update(user_params))
+      redirect_to "/"
+    else
+      render edit_user_path
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+      redirect_to "/"
+  end
+
+
+
+
+
+  private
+	 def user_params
+	  params.require(:user).permit(:name, :password)
+	 end
+
+
+
+end
