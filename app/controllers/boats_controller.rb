69843cadcaf3048ba7e136f03e7821830317a51b
Upshur06 <mupshur2005@gmail.com>
2018-06-12 12:57:55 -0400

    test_controllers

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -1,10 +1,32 @@
 class BoatsController < ApplicationController
   def index
+    @boats = Boat.all
   end
 
   def new
+    @boat = Boat.new
+  end
+
+  def create
+    @boat = Boat.new(boat_params)
+      if boat.save
+        redirect_to "/boats"
+      else
+        render "/boats/new"
+      end
   end
 
   def show
+
   end
+
+  def destroy
+
+  end
+
+  private
+
+  def boat_params
+    params.require(:boat).permit(:name, :containers, :location)
+end
 end
