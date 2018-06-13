04d39dccecf4be36218016b9d07052d7fb14ed19
Upshur06 <mupshur2005@gmail.com>
2018-06-12 23:46:45 -0400

    to_validate

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,6 +2,6 @@ class Boat < ApplicationRecord
 	has_many :jobs
 	has_many :users, :through => :jobs
 
-
+	validates :name, uniqueness: true
 
 end
--- app/models/job.rb
+++ app/models/job.rb
@@ -3,5 +3,5 @@ class Job < ApplicationRecord
 	belongs_to :users
 	validates :title, uniqueness: true
 	validates :title, :info, :origin, :destination, :cost, :containers, :boat_id, :user_id, presence: true
-	
+
 end
