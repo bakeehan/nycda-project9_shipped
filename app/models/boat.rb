abe96f786907daad6e3074e7e7cbe5e4e8a89f08
EBien-Aime <ebienaime@uarts.edu>
2018-06-13 14:25:12 -0400

    added paperclip

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -1,8 +1,10 @@
 class Boat < ApplicationRecord
+	has_attached_file :image, styles: { large: "600x660>", medium: "300x300>", thumb: "100x100#" }
+	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 	has_many :jobs
 	has_many :users, :through => :jobs
 
 	validates :name, uniqueness: true
-	validates :name, :containers, :location, presence: true 
+	validates :name, :containers, :location, presence: true
 
 end
